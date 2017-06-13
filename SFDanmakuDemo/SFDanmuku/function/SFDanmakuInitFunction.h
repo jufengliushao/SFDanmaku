90312ff3f3201ab6a6d5a262a13cf63354587676
liushaofeng <jufengliushao@163.com>
2017-06-13 15:16:25 +0800

    create init

--- SFDanmakuDemo/SFDanmuku/function/SFDanmakuInitFunction.h
+++ SFDanmakuDemo/SFDanmuku/function/SFDanmakuInitFunction.h
@@ -7,7 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SFBaseDanmukuView.h"
+
+typedef NS_ENUM(NSUInteger, SFDM_INIT_TYPE) {
+    SFDM_INIT_TYPE_NIB = 1, /* nib文件类型 */
+    SFDM_INIT_TYPE_CODE /* code文件类型 */
+};
 
 @interface SFDanmakuInitFunction : NSObject
 
+/**
+ 初始化通用方法
+
+ @return 实例对象
+ */
++ (instancetype)shareInstance;
+
+/**
+ 注册文件
+
+ @param fileName 文件名
+ @param type 类型
+ */
+- (void)dm_registerFile:(NSString *)fileName type:(SFDM_INIT_TYPE)type;
+
+/**
+ 返回对应文件名的类型
+
+ @param file 文件名
+ @return SFDM_INIT_TYPE
+ */
+- (SFDM_INIT_TYPE)dm_returnFileType:(NSString *)file;
 @end
--- SFDanmakuDemo/SFDanmuku/function/SFDanmakuInitFunction.m
+++ SFDanmakuDemo/SFDanmuku/function/SFDanmakuInitFunction.m
@@ -8,6 +8,73 @@
 
 #import "SFDanmakuInitFunction.h"
 
-@implementation SFDanmakuInitFunction
+@interface SFDanmakuInitFunction()
 
+/**
+ nib文件数组
+ */
+@property (nonatomic, strong) NSMutableArray *dm_nibArr;
+
+/**
+ 代码文件数组
+ */
+@property (nonatomic, strong) NSMutableArray *dm_codeArr;
+@end
+
+SFDanmakuInitFunction *initF = nil;
+
+@implementation SFDanmakuInitFunction
+#pragma mark - system method
++ (instancetype)shareInstance{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if (!initF) {
+            initF = [[SFDanmakuInitFunction alloc] init];
+        }
+    });
+    return initF;
+}
+
+#pragma mark - public method
+- (void)dm_registerFile:(NSString *)fileName type:(SFDM_INIT_TYPE)type{
+    if ([self dm_returnFileType:fileName]) {
+        // 如果文件已经注册过，不进行添加
+        return;
+    }
+    
+    if (type == SFDM_INIT_TYPE_NIB) {
+        // nib
+        [self.dm_nibArr addObject:fileName];
+    }else if (type == SFDM_INIT_TYPE_CODE){
+        // code
+        [self.dm_codeArr addObject:fileName];
+    }
+}
+
+- (SFDM_INIT_TYPE)dm_returnFileType:(NSString *)file{
+    if ([self.dm_codeArr containsObject:file]) {
+        return SFDM_INIT_TYPE_CODE;
+    }else if ([self.dm_nibArr containsObject:file]){
+        return SFDM_INIT_TYPE_NIB;
+    }
+    return -1;
+}
+
+
+
+#pragma mark - private method
+- (NSMutableArray *)dm_nibArr{
+    if (!_dm_nibArr) {
+        _dm_nibArr = [NSMutableArray arrayWithCapacity:0];
+    }
+    return _dm_nibArr;
+}
+
+
+- (NSMutableArray *)dm_codeArr{
+    if (!_dm_codeArr) {
+        _dm_codeArr = [NSMutableArray arrayWithCapacity:0];
+    }
+    return _dm_codeArr;
+}
 @end
