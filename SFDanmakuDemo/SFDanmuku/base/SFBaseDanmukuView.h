cc4177b4a01bfe48a3fa2bccf025777544362c62
liushaofeng <jufengliushao@163.com>
2017-06-13 11:32:38 +0800

    create baseView

---
+++ SFDanmakuDemo/SFDanmuku/base/SFBaseDanmukuView.h
@@ -0,0 +1,18 @@
+//
+//  SFBaseDanmukuView.h
+//  SFDanmakuDemo
+//
+//  Created by cnlive-lsf on 2017/6/13.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFBaseDanmukuView : UIView
+
+/**
+ 弹幕view的重用标识
+ */
+@property (nonatomic, copy) NSString *reuseIdentifier;
+
+@end
---
+++ SFDanmakuDemo/SFDanmuku/base/SFBaseDanmukuView.m
@@ -0,0 +1,26 @@
+//
+//  SFBaseDanmukuView.m
+//  SFDanmakuDemo
+//
+//  Created by cnlive-lsf on 2017/6/13.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFBaseDanmukuView.h"
+
+@implementation SFBaseDanmukuView
+- (instancetype)init{
+    if (self = [super init]) {
+        self.reuseIdentifier = @"";
+    }
+    return self;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
