3f8bce7c5bec59b3956167a630a01474ceadaa3c
liushaofeng <jufengliushao@163.com>
2017-06-13 15:18:19 +0800

    create main function files

---
+++ SFDanmakuDemo/SFDanmuku/function/SFDanMuFunction.h
@@ -0,0 +1,13 @@
+//
+//  SFDanMuFunction.h
+//  SFDanmakuDemo
+//
+//  Created by cnlive-lsf on 2017/6/13.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFDanMuFunction : NSObject
+
+@end
---
+++ SFDanmakuDemo/SFDanmuku/function/SFDanMuFunction.m
@@ -0,0 +1,13 @@
+//
+//  SFDanMuFunction.m
+//  SFDanmakuDemo
+//
+//  Created by cnlive-lsf on 2017/6/13.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFDanMuFunction.h"
+
+@implementation SFDanMuFunction
+
+@end
